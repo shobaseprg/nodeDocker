7b650c3d4d1db0a7d87074d1ad06060a1e499ef0
shobaseprg <pgymlsho19128@gmail.com>
2020-11-07 00:21:15 +0900

    onsubmit

--- src/public/javascript/validator.js
+++ src/public/javascript/validator.js
@@ -1,8 +1,27 @@
 const checkForm = (e) => {
   const nameValue = document.getElementById("NameForm").value;
+  const mailValue = document.getElementById("EmailForm").value;
   const passwordValue = document.getElementById("PasswordForm").value;
   const confirmPasswordValue = document.getElementById("confirmPasswordForm").value;
-    if (!(nameValue) || !(passwordValue) || !(confirmPasswordValue)) {
-    console.log('error');
+  const btn = document.getElementById("btn");
+  let cancel = "off";
+    if (!(nameValue)) {
+      document.getElementById('error-name-null').classList.remove("d-none");
+      cancel = "on";
+
+    }
+    if (!(mailValue)) {
+      console.log('error-mail-null');
+    }
+    if (!(passwordValue)) {
+      console.log('error-password-null');
+    }
+    if (!(passwordValue === confirmPasswordValue)) {
+      console.log('error-password-diff');
+    }
+    if (cancel === "on"){
+      return false;
+    } else {
+      return true;
     }
 }
--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   res.render('home');
 });
 
