2b31397600524042e419733b4420f0f8551c04b6
shobaseprg <pgymlsho19128@gmail.com>
2020-11-07 13:39:26 +0900

    displayName

--- src/public/javascript/validator.js
+++ src/public/javascript/validator.js
@@ -6,38 +6,38 @@ const checkForm = (e) => {
   const btn = document.getElementById("btn");
   let cancel = "off";
 
-  document.getElementById('error-name-null').classList.add("d-none");
-  document.getElementById('error-mail-null').classList.add("d-none");
-  document.getElementById('error-password-null').classList.add("d-none");
-  document.getElementById('differencePassword').classList.add("d-none");
-  document.getElementById('error-password-under7').classList.add("d-none");
-
+  document.getElementById("error-name-null").classList.add("d-none");
+  document.getElementById("error-mail-null").classList.add("d-none");
+  document.getElementById("error-password-null").classList.add("d-none");
+  document.getElementById("differencePassword").classList.add("d-none");
+  document.getElementById("error-password-under7").classList.add("d-none");
 
     // 名前空欄
     if (!(nameValue)) {
-      document.getElementById('error-name-null').classList.remove("d-none");
+      document.getElementById("error-name-null").classList.remove("d-none");
       cancel = "on";
     }
     // メール空欄
     if (!(mailValue)) {
-      document.getElementById('error-mail-null').classList.remove("d-none");
+      document.getElementById("error-mail-null").classList.remove("d-none");
       cancel = "on";
     }
     // パスワード空欄
     if (!(passwordValue)) {
-      document.getElementById('error-password-null').classList.remove("d-none");
+      document.getElementById("error-password-null").classList.remove("d-none");
       cancel = "on";
     } else {
       // パスワード７文字以下
       if ((passwordValue.length < 7)) {
-        document.getElementById('error-password-under7').classList.remove("d-none");
+        document.getElementById("error-password-under7").classList.remove("d-none");
         cancel = "on";
       }
     }
     // パスワード不一致
     if (!(passwordValue === confirmPasswordValue)) {
-      document.getElementById('differencePassword').classList.remove("d-none");
-      cancel = "on";    }
+      document.getElementById("differencePassword").classList.remove("d-none");
+      cancel = "on";
+      }
     if (cancel === "on"){
       return false;
     } else {
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-  res.render('home');
+router.post("/", function(req, res, next) {
+  res.render("home", {name: req.body.name});
 });
 
 module.exports = router;
