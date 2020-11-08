4b77e78f2dda28994564426999dbdb59d8327855
shobaseprg <pgymlsho19128@gmail.com>
2020-11-08 13:56:30 +0900

    check all empty

--- src/routes/home.js
+++ src/routes/home.js
@@ -6,11 +6,16 @@ const { check, validationResult } = require('express-validator');
 /* GET users listing. */
 router.post("/", [
   // checkメソッドを使用してバリデーションを実行
-  check('name').not().isEmpty()
+  check('name').not().isEmpty(),
+  check('email').not().isEmpty(),
+  check('password').not().isEmpty(),
+  check('confirmPassword').not().isEmpty()
 ],
   function (req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      console.log(errors.array())
+
       return res.status(422).json({ errors: errors.array() });
     }
 
